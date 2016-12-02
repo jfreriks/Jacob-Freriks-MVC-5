1e16f2eae5a8a74103cfeaa4e1f6de9e5715ef16
Jacob Freriks <jacobf09@live.com>
2016-12-02 16:25:01 -0500

    MVC5 - Demo
    First Upload

---
+++ Jacob Freriks MVC-5/Demo/Demo/Controllers/EnlistController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class EnlistController : Controller
+    {
+        private InfoDBContext db = new InfoDBContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Info.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoDB infoDB = db.Info.Find(id);
+            if (infoDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoDB);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Address,Birthdate")] InfoDB infoDB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Info.Add(infoDB);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(infoDB);
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoDB infoDB = db.Info.Find(id);
+            if (infoDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,Birthdate")] InfoDB infoDB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(infoDB).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(infoDB);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InfoDB infoDB = db.Info.Find(id);
+            if (infoDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InfoDB infoDB = db.Info.Find(id);
+            db.Info.Remove(infoDB);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Jacob Freriks MVC-5/Demo/Demo/Models/InfoDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Demo.Models
+{
+    public class InfoDB
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Birthdate { get; set; }
+
+    }
+    public class InfoDBContext : DbContext
+    {
+        public DbSet<InfoDB> Info { get; set; }
+    }
+}
